24b7524cda9fec355d50603f539a6c696ecbb914
Pixoll <pixoll.gamer@gmail.com>
2024-03-28 21:39:34 -0300

    Forgot to update assignment regex

--- src/commands/removeCert.ts
+++ src/commands/removeCert.ts
@@ -18,7 +18,7 @@ import { getSubjectName } from './certs';
 const assignmentTypes = Object.values(AssignmentType).map(v => capitalize(v));
 const assignmentStringRegex = new RegExp(
     `^\\[(?<type>${assignmentTypes.join('|')})\\] `
-    + /(?<subjectCode>\d+) /.source
+    + /(?<subjectCode>\d+) \w+ /.source
     + /\((?<dueDate>\d{2}\/\d{2}\/\d{4})\)/.source
 );
 
